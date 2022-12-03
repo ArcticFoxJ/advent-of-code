87175ae93104ead12b17f7eb438ff902b3b04198
Jessica <j.j.isaacs@hotmail.co.uk>
2022-12-03 02:25:07 +0000

    day 1 part 2

--- 2022/1_calorie-counting/solution.js
+++ 2022/1_calorie-counting/solution.js
@@ -2,22 +2,28 @@ const path = require('path');
 const { readFileSync } = require('fs');
 
 const maxTotalCalories = (calories) => {
-    const elfCalories = calories
-        .filter(x => !x.match(/^\r?\n$/))
-        .map(x => x
-            .split(/\r?\n/)
-            .reduce((prev, current) => prev + parseInt(current), 0)
-        )
+    return Math.max(...calories)
+}
 
-    return Math.max(...elfCalories)
+const maxThreeTotalCalories = (calories) => {
+    return calories
+        .sort((a, b) => b - a)
+        .slice(0, 3)
+        .reduce((prev, current) => prev + parseInt(current), 0)
 }
 
 module.exports = logSolution = () => {
 
     const calories = readFileSync(path.join(__dirname, 'input.txt'))
         .toString()
-        .split(/(\r?\n){2}/);
+        .split(/(\r?\n){2}/)
+        .filter(x => !x.match(/^\r?\n$/))
+        .map(x => x
+            .split(/\r?\n/)
+            .reduce((prev, current) => prev + parseInt(current), 0)
+        );
 
     console.log('Day 1: Calorie Counting')
     console.log('part 1 => ', maxTotalCalories(calories));
+    console.log('part 2 => ', maxThreeTotalCalories(calories));
 }
