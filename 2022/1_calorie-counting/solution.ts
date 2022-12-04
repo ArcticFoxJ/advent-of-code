78c210a88ac02325f240d6d232857d70da6a6faf
Jessica <j.j.isaacs@hotmail.co.uk>
2022-12-04 14:44:26 +0000

    update ts file with type declarations

--- 2022/1_calorie-counting/solution.ts
+++ 2022/1_calorie-counting/solution.ts
@@ -1,29 +1,29 @@
 import { readFileSync } from 'fs'
 import { join }from 'path'
 
-const maxTotalCalories = (calories) => {
+const maxTotalCalories = (calories: number[]): number => {
     return Math.max(...calories)
 }
 
-const maxThreeTotalCalories = (calories) => {
+const maxThreeTotalCaloriesTotal = (calories: number[]): number => {
     return calories
         .sort((a, b) => b - a)
         .slice(0, 3)
-        .reduce((prev, current) => prev + parseInt(current), 0)
+        .reduce((prev: number, current: number) => prev + current, 0)
 }
 
 export const logSolution = () => {
 
-    const calories = readFileSync(join(__dirname, 'input.txt'))
+    const elfCalorieTotals: number[] = readFileSync(join(__dirname, 'input.txt'))
         .toString()
         .split(/(\r?\n){2}/)
         .filter(x => !x.match(/^\r?\n$/))
         .map(x => x
             .split(/\r?\n/)
-            .reduce((prev, current) => prev + parseInt(current), 0)
+            .reduce((prev: number, current: string) => prev + parseInt(current), 0)
         )
 
     console.log('Day 1: Calorie Counting')
-    console.log('part 1 => ', maxTotalCalories(calories))
-    console.log('part 2 => ', maxThreeTotalCalories(calories))
+    console.log('part 1 => ', maxTotalCalories(elfCalorieTotals))
+    console.log('part 2 => ', maxThreeTotalCaloriesTotal(elfCalorieTotals))
 }
